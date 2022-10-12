d4958cfc8e959fb30a4bad40041d89aa6e42787a
Shreyas Rasaikar <shreyas.rasaikar@infuse.it>
2022-10-12 16:28:33 +0530

    added coupon feature

--- backend/app.py
+++ backend/app.py
@@ -5,18 +5,22 @@ from models.room import Room
 from models.add_on import AddOns
 from routes.rooms_route import rooms_route
 from routes.halls_route import halls_route
+from routes.coupon_route import coupons_route
 
 
 
 app = Flask(__name__)
 app.register_blueprint(rooms_route)
 app.register_blueprint(halls_route)
+app.register_blueprint(coupons_route)
 CORS(app)
 
 
 
 @app.route('/')
 def index():
+
+
     return "Hello World"
 
 if __name__ == '__main__':
