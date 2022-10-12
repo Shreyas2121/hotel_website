8af559d5ac5f33cd2e72d1fe5fe94b199505f531
Shreyas Rasaikar <shreyas.rasaikar@infuse.it>
2022-10-12 12:39:23 +0530

    f

--- frontend/src/pages/Rooms.tsx
+++ frontend/src/pages/Rooms.tsx
@@ -3,7 +3,9 @@ import { UseFetch } from "../customHook/UseFetch";
 import { Room } from "../types/types";
 
 interface Res {
-  data: Room;
+  data: {
+    rooms: Room[];
+  };
   loading: boolean;
 }
 
@@ -13,6 +15,7 @@ const Rooms = () => {
     "http://127.0.0.1:5000/booking/room/getDetails"
   );
   // roomData = res.data;
+  console.log(data.rooms[0].room_id);
 
   return <div style={{ minHeight: "100vh" }}>Rooms</div>;
 };
