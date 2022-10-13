a7835ef58f541eba51e99411395cd4b571300973
Atharva Malpekar <96663591+Gekky-Pro-grammer@users.noreply.github.com>
2022-10-13 10:56:20 +0530

    updated hall

--- frontend/src/pages/Halls.tsx
+++ frontend/src/pages/Halls.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import {Container}  from "react-bootstrap";
+import Service from "../components/services/Service";
+import RenderHall from "../components/image_renderer_hall/RenderHall";
 
 const Halls = () => {
-  return <div style={{ minHeight: "100vh" }}>Halls</div>;
+  return (
+  <div style={{ minHeight: "100vh" }}>
+  <Container>
+  <br />
+  <RenderHall />
+  <br />
+
+  </Container>
+  </div>
+
+  );
 };
 
 export default Halls;
