6b8870d91874c3e7870e7c0307b1a8084e025735
Shreyas Rasaikar <shreyas.rasaikar@infuse.it>
2022-10-14 17:43:14 +0530

    fixed some bugs

---
+++ backend/tests/coupon_route_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+from models.coupon import Coupon
+
+
+def test_get_coupon():
+    couponObj = Coupon.objects()
+    assert couponObj is not None
+
+def test_check_coupon():
+    coupon_res = "WELCOME10"
+    for coupan in Coupon.objects():
+        try:
+            if coupan.coupons[coupon_res]:
+                assert coupan.coupons[coupon_res] == 10
+        except:
+            assert "Invalid Coupon" == "Invalid Coupon"
+    assert "Hello" == "Hello"
