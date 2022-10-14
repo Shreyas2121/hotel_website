92177a5e56cba9378601a4b5b6986b76b245ec97
Shreyas Rasaikar <shreyas.rasaikar@infuse.it>
2022-10-14 17:04:43 +0530

    added availability func

--- frontend/src/components/Roomcards/Roomcards.tsx
+++ frontend/src/components/Roomcards/Roomcards.tsx
@@ -4,16 +4,34 @@ import "./roomcards.css";
 import { Room } from "../../types/types";
 import { Link, useNavigate } from "react-router-dom";
 
+interface Status {
+  Basic: number;
+  Suite: number;
+  Deluxe: number;
+}
 interface Props {
   roomData: Room;
   checkin: Date;
   checkout: Date;
+  status: Status;
 }
 
-const Roomcards = ({ roomData, checkin, checkout }: Props) => {
+const Roomcards = ({ roomData, checkin, checkout, status }: Props) => {
   const [no, setNo] = useState(0);
   const roomType = roomData.room_type;
   const roomPrice = Number(roomData.room_price);
+
+  const check = () => {
+    console.log(status);
+    let keys = Object.keys(status);
+    if (keys.includes(roomType)) {
+      if (status[roomType] > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
 
   return (
     <div className="tm-rooms">
@@ -25,21 +43,31 @@ const Roomcards = ({ roomData, checkin, checkout }: Props) => {
         <p className="roomdesc">Description: {roomData.room_desc}</p>
         <p className="roomamenities">Amenities: {roomData.room_amenties}</p>
       </div>
-      <p id="status">STATUS : Availabel/Not Available</p>
+      {check() ? <div>Available</div> : <div>Not Available</div>}
 
       <div id="no_of_rooms">
-        <button className="no_of_rooms_btn" onClick={(e) => setNo((prev) => prev - 1)}>-</button>
+        <button
+          className="no_of_rooms_btn"
+          onClick={(e) => setNo((prev) => prev - 1)}
+        >
+          -
+        </button>
         <span>{no}</span>
-        <button className="no_of_rooms_btn" onClick={(e) => setNo((prev) => prev + 1)}>+</button>
+        <button
+          className="no_of_rooms_btn"
+          onClick={(e) => setNo((prev) => prev + 1)}
+        >
+          +
+        </button>
       </div>
 
       <p id="booknow">
-      <Link
-        to="/booking"
-        state={{ no, checkin, checkout, roomType, roomPrice }}
-      >
-        Book Now
-      </Link>
+        <Link
+          to="/booking"
+          state={{ no, checkin, checkout, roomType, roomPrice }}
+        >
+          Book Now
+        </Link>
       </p>
     </div>
   );
