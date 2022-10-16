cafd4a444d5887aeeeadf80516eb5f3b640897dd
Gekky-Pro-grammer <malpekaratharv@gmail.com>
2022-10-16 17:35:22 +0530

    Added fixed post data issue of add review page

--- backend/controller/routes/reviews_route.py
+++ backend/controller/routes/reviews_route.py
@@ -5,10 +5,10 @@ from controller.actions.reviews_action import get_reviews,add_reviews
 reviews_route = Blueprint('reviews_route', __name__)
 
 
-@reviews_route.route('/reviews',methods=['GET'])
+@reviews_route.route('/reviews/',methods=['GET'])
 def get():
     return get_reviews()
 
-@reviews_route.route('/reviews',methods=['POST'])
+@reviews_route.route('/reviews/',methods=['POST'])
 def add():
     return add_reviews()
