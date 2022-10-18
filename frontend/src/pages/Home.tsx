0a63657c2348f63c6c92989b8dd582c875c3c233
Shreyas Rasaikar <shreyas.rasaikar@infuse.it>
2022-10-18 17:25:00 +0530

    fixed

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,21 +6,24 @@ import Reviewcards from "../components/Roomcards/Reviewcards";
 import bannerImage from "../assets/images/banner_bg.jpg";
 
 const Home = () => {
-  
   return (
     <header>
       <div
-        className='p-5 text-center bg-image'
+        className="p-5 text-center bg-image"
         style={{ backgroundImage: `url(${bannerImage})`, height: "45rem" }}
-        >
-        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
-          <div className='d-flex justify-content-center align-items-center h-100'>
-            <div className='text-white'>
-              <h1 className='mb-2' style={{
-                font: "5rem bold",
-              }}
-              >Relax Your Mind</h1>
-              <h6 className='mb-3'>AWAY FROM MONOTONOUS LIFE</h6>
+      >
+        <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}>
+          <div className="d-flex justify-content-center align-items-center h-100">
+            <div className="text-white">
+              <h1
+                className="mb-2"
+                style={{
+                  font: "5rem bold",
+                }}
+              >
+                A Best Place To Stay
+              </h1>
+              <h6 className="mb-3">MAKE YOUR VACATION HAPPY</h6>
               {/* <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
                 Call to action
               </a> */}
@@ -28,25 +31,26 @@ const Home = () => {
           </div>
         </div>
 
-        <div style={{
-          position: "absolute",
-          bottom: "10%",
-          right: "2%",
-          color: "white",
-        }}
+        <div
+          style={{
+            position: "absolute",
+            bottom: "10%",
+            right: "2%",
+            color: "white",
+          }}
         >
-          SCROLL 
-          <br/>DOWN
-          <br/>↓↓↓
+          SCROLL
+          <br />
+          DOWN
+          <br />
+          ↓↓↓
         </div>
-
       </div>
 
       <PhotoGrid />
       <Service />
       <br />
       <Reviewcards />
-
     </header>
   );
 };
