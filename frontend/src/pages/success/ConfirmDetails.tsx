43d6b330926d46860cef521ee416d7ae629e7976
Shreyas Rasaikar <shreyas.rasaikar@infuse.it>
2022-10-20 14:36:31 +0530

    fixed css

--- frontend/src/pages/success/ConfirmDetails.tsx
+++ frontend/src/pages/success/ConfirmDetails.tsx
@@ -4,7 +4,9 @@ import { useLocation, useNavigate } from "react-router-dom";
 import "./success.css";
 
 const ConfirmDetails = () => {
-  const { state } = useLocation();
+  const {
+    state: { data, key },
+  } = useLocation();
 
   const navigate = useNavigate();
 
@@ -14,31 +16,46 @@ const ConfirmDetails = () => {
         <h5 className="card-title">Your Booking is confirmed!!</h5>
         <p className="card-text">
           <span>Name: </span>
-          {state.name}
+          {data.name}
         </p>
         <p className="card-text">
           <span>Email: </span>
-          {state.email}
+          {data.email}
         </p>
         <p className="card-text">
           <span>Check-in: </span>
-          {state.checkin?.toDateString()}
+          {data.checkin?.toDateString()}
         </p>
         <p className="card-text">
           <span>Check-out: </span>
-          {state.checkout?.toDateString()}
+          {data.checkout?.toDateString()}
         </p>
         <p className="card-text">
           <span>Room Type: </span>
-          {state.roomType}
+          {data.roomType}
         </p>
         <p className="card-text">
           <span>Total Price: </span>
-          {state.total}
+          {data.total}
         </p>
         <button onClick={() => navigate("/")} id="btn">
           Go Home
         </button>
+        {key == "Hall" ? (
+          <div></div>
+        ) : (
+          <p>
+            <small
+              style={{
+                marginLeft: "3.5rem",
+                marginTop: "1rem",
+              }}
+              className=""
+            >
+              *Check-in: 12:00 PM, Check-out: 11:00 AM
+            </small>
+          </p>
+        )}
       </div>
     </div>
   );
